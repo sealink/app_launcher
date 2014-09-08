650198f4649c1da9dfab0cfa10e2532872397505
Adam Davies <adzdavies@gmail.com>
2014-09-08 12:43:02 +0930

    Fix status check -- now use capture3 and exit_status

--- app/models/service_status.rb
+++ app/models/service_status.rb
@@ -4,11 +4,8 @@ ServiceStatus = Struct.new(:ok, :message) do
 
   # Return a new status instance for the given command
   def self.for(command)
-    stdin, stdout, stderr = Open3.popen3(command)
-    error_message = stderr.read
-
-    ok = error_message['Already running'] || error_message['Running with PID']
-    new(ok, error_message)
+    stdout, stderr, exit_status = Open3.capture3(command)
+    new(exit_status.success?, stdout + stderr)
   end
 
   def running?
